4c684359b3f19fdb46df817525075653c44f487b
Andrew Carroll <andrewc411@yahoo.com>
2022-11-22 19:39:56 -0500

    add more img, filling arrays w/img

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-// const tshirt = []
+const tshirt = [
+    'img/jcole.webp',
+    'img/gorillaz.webp',
+    'img/no_face.webp',
+    'img/ratm.webp',
+    'img/pulpfiction.avif',
+    'img/jcoleisbetter.webp',
+    'img/immortal.webp'
+]
 // const longSleeve = []
 // const shorts = []
 // const pants = []
@@ -7,11 +15,47 @@
 // const sweatshirt = []
 // const hoodie = []
 // const jacket = []
-// const socks = []
-// const shoes = []
-// const hat = []
+const socks = [
+    'img/champion_sox.jpg',
+    'img/idc_sox.jpg',
+    'img/mf_sox.jpg',
+    'img/neutral_sox.webp',
+    'img/retrostripetube_sox.jpg',
+    'img/tiedye_sox.webp',
+    'img/wool_sox.webp'
+]
+const shoes = [
+    'img/airmax90.jpg',
+    'img/darknightadidas.jpg',
+    'img/hiking_boots.jpg',
+    'img/nautica_boots.jpg',
+    'img/newbalance.jpg',
+    'img/nikecartoon.webp',
+    'img/nikehuarache.jpg',
+    'img/nikesbdunks.jpg',
+    'img/redbottom_heels.webp',
+    'img/vans.jpg'
+]
+const hat = [
+    'img/nasa_hat.webp',
+    'img/nujabes_hat.webp',
+    'img/nintendo_hat.webp',
+    'img/ghostType.webp',
+    'img/atribecalled_.webp',
+    'img/onmute_hat.webp',
+    'img/bucket_hat.webp',
+    'img/akatsuki_bucket.webp',
+    'img/alienAbduction.webp'
+]
 // const sunglasses = []
-// const watch = []
+const watch = [
+    'img/applewatch.jpg',
+    'img/blackgoldcasio.jpg',
+    'img/michaelkors.jpg',
+    'img/stainlessblackface.jpg',
+    'img/strangerthings.jpg',
+    'img/whitecasio.jpg'
+]
 
 const clothes = [
     'img/gorillaz.webp',
